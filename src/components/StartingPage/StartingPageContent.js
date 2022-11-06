d40ac87e03326abdbf48c3fe77ca147d86f7f8f5
Dzenis <110186379+Dzenoo@users.noreply.github.com>
2022-11-06 10:03:21 +0100

    Add files via upload

---
+++ src/components/StartingPage/StartingPageContent.js
@@ -0,0 +1,11 @@
+import classes from "./StartingPageContent.module.css";
+
+const StartingPageContent = () => {
+  return (
+    <section className={classes.starting}>
+      <h1>Starting Page</h1>
+    </section>
+  );
+};
+
+export default StartingPageContent;
